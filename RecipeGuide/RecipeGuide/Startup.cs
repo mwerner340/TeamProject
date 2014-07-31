bf322b854688cbfa44c74a226567eca38e6d0588
mwerner340 <mikewerner@att.net>
2014-07-31 16:03:23 -0400

    Initial Commit
    Initial Commit

---
+++ RecipeGuide/RecipeGuide/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecipeGuide.Startup))]
+namespace RecipeGuide
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
